8dbab09c3a04034b0d5f28c305ad3cbe2ec9210f
blaszen <blaszen.dev@gmail.com>
2025-06-10 14:39:04 -0700

    latest UI

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,69 +1,87 @@
-import { StyleSheet, Text, View, TextInput } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, TextInput } from "react-native";
+import React from "react";
 import { Ionicons } from "@expo/vector-icons";
-import { useState } from 'react';
+import { useState } from "react";
 
-type Props = {}
+
+//list of Food categories
+
+const foodGroups = [
+  {
+    id:1,
+    title:"American",
+  },
+  {
+    id:2,
+    title:"Asian",
+  },
+  {
+    id:3,
+    title:"American",
+  },
+  {
+    id:4,
+    title:"American",
+  },
+]
+
+
+type Props = {};
 
 const HomeScreen = (props: Props) => {
-
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
 
   const handleSearch = (query: string) => {
     setSearchQuery(query);
-    console.log('Search query:', query);
+    console.log("Search query:", query);
   };
 
   return (
     <View style={styles.container}>
-        {/* Heading */}
+      {/* Heading */}
       <View style={styles.heading}>
         <Text style={styles.header}>Menyu</Text>
-        <Ionicons name="notifications-outline" size={20} color={'black'}/>
-    </View>
+        <Ionicons name="notifications-outline" size={20} color={"black"} />
+      </View>
       {/* SearchBar */}
-    <View style={styles.searchContainer}>
-    <Ionicons name="search-outline" size={20} color={'black'}/>
+      <View style={styles.searchContainer}>
+        <Ionicons name="search-outline" size={20} color={"black"} />
         <TextInput
           style={styles.input}
-          placeholder="What do you want to eat?"
+          placeholder="Type an ingredient"
           value={searchQuery}
           onChangeText={handleSearch}
           autoCorrect={false}
           clearButtonMode="always"
         />
-            <Ionicons name="close-outline" size={20} color={'black'}/>
+        <Ionicons name="close-outline" size={20} color={"black"} />
       </View>
       {/*Categories */}
       <View style={styles.categories}>
-        <Text>Rice</Text>
+        <Text>American</Text>
       </View>
-
     </View>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
 
 const styles = StyleSheet.create({
   container: {
-   flex:1,
-  marginLeft:10,
-  marginTop:10,
-  marginRight:10
-
-
-    
+    flex: 1,
+    marginLeft: 10,
+    marginTop: 10,
+    marginRight: 10,
   },
-  heading:{
-    justifyContent:'space-between',
-    flexDirection:'row',
-    alignItems:'center'
+  heading: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    alignItems: "center",
   },
 
-  header:{
-    fontSize:24,
-    fontWeight:'bold',
+  header: {
+    fontSize: 24,
+    fontWeight: "bold",
   },
   searchContainer: {
     flexDirection: "row",
@@ -74,25 +92,23 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.1,
     shadowOffset: { width: 0, height: 1 },
     elevation: 2,
-    marginTop:10,
-    paddingLeft:10,
-    paddingRight:10
-
+    marginTop: 10,
+    paddingLeft: 10,
+    paddingRight: 10,
   },
   input: {
     flex: 1,
     fontSize: 16,
   },
-  categories:{
-    marginTop:10,
+  categories: {
+    marginTop: 10,
     height: 40,
-    width:90,
-    borderColor: 'gray',
+    width: 90,
+    borderColor: "gray",
     borderWidth: 1,
     paddingHorizontal: 10,
     borderRadius: 25,
-    alignItems:'center',
-    justifyContent:'center'
-  }
-
-})
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
